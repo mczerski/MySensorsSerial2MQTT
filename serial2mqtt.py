4e090262122dc6e966b92d9c526ed174e1bb253e
Marek Czerski <ma.czerski@gmail.com>
2018-01-25 22:24:00 +0100

    use serial.threaded

--- serial2mqtt.py
+++ serial2mqtt.py
@@ -1,8 +1,56 @@
 import paho.mqtt.client as mqtt
 import serial
+from serial.threaded import ReaderThread, LineReader
 import time
-import threading
 import argparse
+
+class MySerialReader(LineReader):
+    TERMINATOR = b'\n'
+    I_LOG_MESSAGE = '9'
+
+    def __init__(self, rootTopic, mqttClient):
+        super(MySerialReader, self).__init__()
+        self._rootTopic = rootTopic
+        self._mqttClient = mqttClient
+
+    def handle_line(self, line):
+        print(line)
+        fields = line.split(';')
+        if len(fields) != 6:
+            return
+        if fields[4] == self.I_LOG_MESSAGE:
+            return
+        topic = "/".join([self._rootTopic+'out'] + fields[:-1])
+        print('sending topic: %s. payload: %s' % (topic, fields[-1]))
+        self._mqttClient.publish(topic, fields[-1])
+
+class Serial2MQTT:
+    def __init__(self, device, host, port, rootTopic):
+        self._rootTopic = rootTopic
+        self._mqttClient = mqtt.Client()
+        self._mqttClient.connect(host, port)
+        self._mqttClient.subscribe(self._rootTopic + "in/#")
+        self._mqttClient.on_message = self._mqtt_on_message
+        ser = serial.Serial(args.device, 115200, timeout=1)
+        self._serialClient = ReaderThread(ser, lambda: MySerialReader(self._rootTopic, self._mqttClient))
+        self._serialProtocol = None
+
+    def _mqtt_on_message(self, client, obj, msg):
+        payload = msg.payload.decode("utf-8")
+        print ('received topic: %s. payload: %s' % (msg.topic, payload))
+        fields = msg.topic.split('/')
+        data = ";".join(fields[1:] + [payload])
+        print ('writing msg: %s' % data)
+        self._serialProtocol.write_line(data)
+
+    def run(self):
+        self._serialClient.start()
+        _, self._serialProtocol = self._serialClient.connect()
+        self._mqttClient.loop_start()
+
+    def stop(self):
+        self._mqttClient.loop_stop()
+        self._serialClient.stop()
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--broker-host', default='localhost')
@@ -12,44 +60,12 @@ args = parser.parse_args()
 
 rootTopic = 'mySensors'
 
-client = mqtt.Client()
-client.connect(args.broker_host, args.broker_port)
-client.subscribe(rootTopic + "in/#")
-
-ser = serial.Serial(args.device, 115200, timeout=1)
-
-def mqtt_on_message(client, obj, msg):
-    print 'received topic: %s. payload: %s' % (msg.topic, msg.payload)
-    fields = msg.topic.split('/')
-    data = ";".join(fields[1:] + [msg.payload]).encode()
-    print 'writing msg: %s' % data
-    ser.write(data + '\n')
-
-client.on_message = mqtt_on_message
-client.loop_start()
-
-run =[True]
-
-def read_from_port():
-    while run[0]:
-        reading = ser.readline().decode().strip()
-        if not reading:
-            continue
-        print reading.strip()
-        fields = reading.split(';')
-        if len(fields) != 6:
-            continue
-        print 'received msg: %s' % reading
-        topic = "/".join([rootTopic+'out'] + fields[:-1])
-        print 'sending topic: %s. payload: %s' % (topic, fields[-1])
-        client.publish(topic, fields[-1])
-
-thread = threading.Thread(target=read_from_port)
-thread.start()
+serial2Mqtt = Serial2MQTT(args.device, args.broker_host, args.broker_port, rootTopic)
+serial2Mqtt.run()
 
 try:
     while True:
         time.sleep(100)
 except:
-    run[0] = False
-    client.loop_stop()
+    serial2Mqtt.stop()
+
